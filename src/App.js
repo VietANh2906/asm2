20dcd49353c0afe975362c4f31db0a965627f3a5
quanvx4 <quanvx@msb.com.vn>
2025-10-22 14:14:54 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [stats, setStats] = useState({ total_orders: 0, total_amount: 0 });
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/stats")
+      .then((res) => res.json())
+      .then((data) => setStats(data))
+      .catch((err) => console.error("Lỗi khi gọi API:", err));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: "30px", fontFamily: "Arial" }}>
+      <h1>📊 Thống kê đơn hàng</h1>
+      <p>Tổng số đơn hàng: {stats.total_orders}</p>
+      <p>Tổng doanh thu: {stats.total_amount.toLocaleString()}đ</p>
     </div>
   );
 }
